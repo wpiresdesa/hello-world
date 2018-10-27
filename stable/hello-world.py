f7ee2130e3606bc0634dfefab64140faa113ebd0
Wanderley Pires de Sa <pires@if.usp.br>
2018-10-27 14:05:35 -0300

    Modifications at hello-world.py using Sublime Text with git add-on - #01

--- stable/hello-world.py
+++ stable/hello-world.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python3
 # _*_ coding: utf-8 _*_
 
+# Main hello-world repository
+# Modification made #01 at /Users/pires/ProgrammingLanguages/LearningGit/hello-world/stable
+
 # This program prints Hello, world!
 
 print('Hello, world!')
